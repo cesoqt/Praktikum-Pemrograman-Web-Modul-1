ede77f76f23b8ff9ce81e9c646fde2c764246ab9
cesoqt <muhammadfikry757@gmail.com>
2026-09-22 11:11:42 +0700

    pertemuan 2 latihan PHP dasar

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/latihan-php', function () {
+    $nama = 'Muhammad Al-Fikry Akbar';
+    $nilai = [60, 75, 55, 70, 80];
+
+    $hitungRataRata = function (array $data): float {
+        $total = 0;
+        foreach ($data as $angka) {
+            $total += $angka;
+        }
+        return $total / count($data);
+    };
+
+    $rataRata = $hitungRataRata($nilai);
+    if ($rataRata >= 75) {
+        $status = 'Lulus';
+    } else {
+        $status = 'Perlu Perbaikan';
+    }
+
+    return view('latihan-php', compact(
+        'nama', 'nilai', 'rataRata', 'status'
+    ));
 });
